2896a307b57742fd43ef2eae3239ddfdbb62a383
Justin Pridgen <justincpridgen672@gmail.com>
2024-10-12 21:14:02 -0400

    I'm back

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
 # Developer Badges Changelog
+## v1.0.1 (2024-10-12)
+- Added the ability to customize comment coloring based on a user's badges
+- Fixed a bug where reloading the page would add duplicate badges to a user's profile
+
 ## v1.0.0 (2024-10-01)
 - Initial release
--- src/main.cpp
+++ src/main.cpp
@@ -24,37 +24,36 @@ bool TRIED_LOADING = false;
 
 #define PROPERTY_OR_DEFAULT(obj, prop, isFunc, asFunc, def) (obj.contains(prop) && obj[prop].isFunc() ? obj[prop].asFunc() : def)
 
-void showBadgeInfo(std::string username, CCMenuItemSpriteExtra* badgeButton) {
-    auto badge = BadgeType::None;
-    auto badgeRect = static_cast<CCSprite*>(badgeButton->getNormalImage())->getTextureRect();
-    auto sfc = CCSpriteFrameCache::sharedSpriteFrameCache();
-    if (badgeRect == sfc->spriteFrameByName("badge01.png"_spr)->getRect()) badge = BadgeType::ModDeveloper;
-    else if (badgeRect == sfc->spriteFrameByName("badge02.png"_spr)->getRect()) badge = BadgeType::VerifiedDeveloper;
-    else if (badgeRect == sfc->spriteFrameByName("badge03.png"_spr)->getRect()) badge = BadgeType::IndexStaff;
-    else if (badgeRect == sfc->spriteFrameByName("badge04.png"_spr)->getRect()) badge = BadgeType::LeadDeveloper;
-
+void showBadgeInfo(std::string username, CCObject* badgeButton) {
     auto badgeName = "";
-    switch (badge) {
-        case BadgeType::ModDeveloper: badgeName = "Mod Developer"; break;
-        case BadgeType::VerifiedDeveloper: badgeName = "Verified Developer"; break;
-        case BadgeType::IndexStaff: badgeName = "Index Staff"; break;
-        case BadgeType::LeadDeveloper: badgeName = "Lead Developer"; break;
-        default: badgeName = ""; break;
-    }
-
     auto badgeDesc = "";
-    switch (badge) {
-        case BadgeType::ModDeveloper: badgeDesc = "is a <ca>mod developer</c> for <cy>Geode</c>.\n"
+    switch ((BadgeType)badgeButton->getTag()) {
+        case BadgeType::ModDeveloper:
+            badgeName = "Mod Developer";
+            badgeDesc = "is a <ca>mod developer</c> for <cy>Geode</c>.\n"
             "They have created mods that are available on the <cy>Geode mod index</c>.\n"
-            "They will have to have new mods and mod updates approved by the <cd>index staff</c>"; break;
-        case BadgeType::VerifiedDeveloper: badgeDesc = "is a <cp>verified mod developer</c> for <cy>Geode</c>.\n"
+            "They will have to have new mods and mod updates approved by the <cd>index staff</c>";
+            break;
+        case BadgeType::VerifiedDeveloper:
+            badgeName = "Verified Developer";
+            badgeDesc = "is a <cp>verified mod developer</c> for <cy>Geode</c>.\n"
             "They can update their mods on the <cy>Geode mod index</c> without the need for approval.\n"
-            "They will still have to have new mods approved by the <cd>index staff</c>"; break;
-        case BadgeType::IndexStaff: badgeDesc = "is a member of the <cd>index staff</c> for <cy>Geode</c>.\n"
-            "They can approve or reject mods uploaded to the <cy>Geode mod index</c>"; break;
-        case BadgeType::LeadDeveloper: badgeDesc = "is a <co>lead developer</c> of <cy>Geode</c>.\n"
-            "They are part of the main development team and have significant contributions to the <cy>Geode ecosystem</c>"; break;
-        default: badgeDesc = ""; break;
+            "They will still have to have new mods approved by the <cd>index staff</c>";
+            break;
+        case BadgeType::IndexStaff:
+            badgeName = "Index Staff";
+            badgeDesc = "is a member of the <cd>index staff</c> for <cy>Geode</c>.\n"
+            "They can approve or reject mods uploaded to the <cy>Geode mod index</c>";
+            break;
+        case BadgeType::LeadDeveloper:
+            badgeName = "Lead Developer";
+            badgeDesc = "is a <co>lead developer</c> of <cy>Geode</c>.\n"
+            "They are part of the main development team and have significant contributions to the <cy>Geode ecosystem</c>";
+            break;
+        default:
+            badgeName = "";
+            badgeDesc = "";
+            break;
     }
 
     FLAlertLayer::create(badgeName, fmt::format("<cg>{}</c> {}.", username, badgeDesc), "OK")->show();
@@ -62,11 +61,11 @@ void showBadgeInfo(std::string username, CCMenuItemSpriteExtra* badgeButton) {
 
 ccColor3B getCommentColor(BadgeType badge) {
     switch (badge) {
-        case BadgeType::ModDeveloper: return { 163, 121, 192 };
-        case BadgeType::VerifiedDeveloper: return { 202, 134, 159 };
-        case BadgeType::IndexStaff: return { 223, 143, 143 };
-        case BadgeType::LeadDeveloper: return { 225, 178, 155 };
-        default: return { 255, 255, 255 };
+        case BadgeType::ModDeveloper: return Mod::get()->getSettingValue<ccColor3B>("mod-developer-color");
+        case BadgeType::VerifiedDeveloper: return Mod::get()->getSettingValue<ccColor3B>("verified-developer-color");
+        case BadgeType::IndexStaff: return Mod::get()->getSettingValue<ccColor3B>("index-staff-color");
+        case BadgeType::LeadDeveloper: return Mod::get()->getSettingValue<ccColor3B>("lead-developer-color");
+        default: return ccColor3B { 255, 255, 255 };
     }
 }
 
@@ -94,7 +93,7 @@ class $modify(DBMenuLayer, MenuLayer) {
                 }
             }
             else Loader::get()->queueInMainThread([] {
-                Notification::create("Developer Badges load failed", NotificationIcon::Error)->show();
+                Notification::create("Developer Badges Load Failed", NotificationIcon::Error)->show();
             });
 
             task = std::nullopt;
@@ -109,8 +108,14 @@ class $modify(DBMenuLayer, MenuLayer) {
 
 #include <Geode/modify/ProfilePage.hpp>
 class $modify(DBProfilePage, ProfilePage) {
+    struct Fields {
+        bool m_hasBadge;
+    };
+
     void loadPageFromUserInfo(GJUserScore* score) {
         ProfilePage::loadPageFromUserInfo(score);
+
+        if (m_fields->m_hasBadge) return;
 
         auto usernameMenu = m_mainLayer->getChildByID("username-menu");
         if (!usernameMenu) return;
@@ -127,12 +132,15 @@ class $modify(DBProfilePage, ProfilePage) {
             menu_selector(DBProfilePage::onBadge)
         );
         badgeButton->setID("developer-badge"_spr);
+        badgeButton->setTag((int)badge.badge);
         usernameMenu->addChild(badgeButton);
         usernameMenu->updateLayout();
+
+        m_fields->m_hasBadge = true;
     }
 
     void onBadge(CCObject* sender) {
-        showBadgeInfo(m_score->m_userName, static_cast<CCMenuItemSpriteExtra*>(sender));
+        showBadgeInfo(m_score->m_userName, sender);
     }
 };
 
@@ -158,10 +166,15 @@ class $modify(DBCommentCell, CommentCell) {
             menu_selector(DBCommentCell::onBadge)
         );
         badgeButton->setID("developer-badge"_spr);
+        badgeButton->setTag((int)badge.badge);
         usernameMenu->addChild(badgeButton);
         usernameMenu->updateLayout();
 
         if (m_comment->m_modBadge >= 1 || m_comment->m_levelID == 0) return;
+        if (badge.badge == BadgeType::ModDeveloper && !Mod::get()->getSettingValue<bool>("mod-developer-color-toggle")) return;
+        if (badge.badge == BadgeType::VerifiedDeveloper && !Mod::get()->getSettingValue<bool>("verified-developer-color-toggle")) return;
+        if (badge.badge == BadgeType::IndexStaff && !Mod::get()->getSettingValue<bool>("index-staff-color-toggle")) return;
+        if (badge.badge == BadgeType::LeadDeveloper && !Mod::get()->getSettingValue<bool>("lead-developer-color-toggle")) return;
 
         if (auto commentTextLabel = static_cast<CCLabelBMFont*>(m_mainLayer->getChildByID("comment-text-label")))
             commentTextLabel->setColor(getCommentColor(badge.badge));
@@ -181,6 +194,6 @@ class $modify(DBCommentCell, CommentCell) {
     }
 
     void onBadge(CCObject* sender) {
-        showBadgeInfo(m_comment->m_userName, static_cast<CCMenuItemSpriteExtra*>(sender));
+        showBadgeInfo(m_comment->m_userName, sender);
     }
 };
